8611619bcee15a26c2eb7a1352fe0e1827a16744
Ben Walton <ben@totaramanor.nz>
2021-07-18 16:38:59 +1200

    Separated Code for Sulfuras

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -4,14 +4,13 @@ namespace csharp
 {
     public class GildedRose
     {
-        
-        const string AgedBrie = "Aged Brie";
+        private const string AgedBrie = "Aged Brie";
 
-        const string BackStagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        private const string BackStagePasses = "Backstage passes to a TAFKAL80ETC concert";
 
-        const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
 
-        readonly IList<Item> _items;
+        private readonly IList<Item> _items;
         public GildedRose(IList<Item> items)
         {
             this._items = items;
@@ -21,47 +20,46 @@ namespace csharp
         {
             foreach (var item in _items)
             {
+                if (item.Name == Sulfuras)
+                    continue;
+
+                item.SellIn -= 1;
+                
                 if (item.Name != AgedBrie && item.Name != BackStagePasses)
                 {
                     if (item.Quality > 0)
                     {
-                        if (item.Name != Sulfuras)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
+                        item.Quality -= 1;
                     }
                 }
                 else
                 {
                     if (item.Quality < 50)
                     {
-                        item.Quality = item.Quality + 1;
+                        item.Quality += 1;
 
                         if (item.Name == BackStagePasses)
                         {
-                            if (item.SellIn < 11)
+                            if (item.SellIn < 10)
                             {
                                 if (item.Quality < 50)
                                 {
-                                    item.Quality = item.Quality + 1;
+                                    item.Quality += 1;
                                 }
                             }
 
-                            if (item.SellIn < 6)
+                            if (item.SellIn < 5)
                             {
                                 if (item.Quality < 50)
                                 {
-                                    item.Quality = item.Quality + 1;
+                                    item.Quality += 1;
                                 }
                             }
                         }
                     }
                 }
-
-                if (item.Name != Sulfuras)
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
+                
+                
 
                 if (item.SellIn < 0)
                 {
@@ -71,22 +69,19 @@ namespace csharp
                         {
                             if (item.Quality > 0)
                             {
-                                if (item.Name != Sulfuras)
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
+                                item.Quality -= 1;
                             }
                         }
                         else
                         {
-                            item.Quality = item.Quality - item.Quality;
+                            item.Quality -= item.Quality;
                         }
                     }
                     else
                     {
                         if (item.Quality < 50)
                         {
-                            item.Quality = item.Quality + 1;
+                            item.Quality += 1;
                         }
                     }
                 }
